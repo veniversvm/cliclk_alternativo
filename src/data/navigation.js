fe3c6662592a29434f08af231f2df03168438bec
FranSabt <hsabatino1@gmail.com>
2025-08-01 16:36:36 -0400

    menu bar

---
+++ src/data/navigation.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { text: 'TODO', href: '/todo' },
+  { text: 'PÁGINAS', href: '/paginas' },
+  { text: 'NOTICIAS', href: '/noticias' },
+  { text: 'GAMING', href: '/gaming' },
+  { text: 'CINE', href: '/cine' },
+  { text: 'MÚSICA', href: '/musica' },
+  { text: 'CONTACTO', href: '/contacto' },
+];
