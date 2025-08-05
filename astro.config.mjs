9cd0a5104debd05fc14db1d93fdd0500d730ab7d
FranSabt <hsabatino1@gmail.com>
2025-08-05 16:35:16 -0400

    trabjando en el footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon"; // <-- Se habrá añadido esta línea
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    icon(), // <-- Y esta otra línea
+    // ...otras integraciones que tengas
+  ]
+});
