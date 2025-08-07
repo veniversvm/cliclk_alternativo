dae1dab3a8f5385edcfcd2e2e5449a0f27a70331
FranSabt <hsabatino1@gmail.com>
2025-08-06 21:09:52 -0400

    nuevos ajustes

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -2,9 +2,12 @@ export const navLinks = [
   { text: 'INICIO', href: '/' },
   { text: 'TODO', href: '/todo' },
   { text: 'PÁGINAS', href: '/paginas' },
-  { text: 'NOTICIAS', href: '/noticias' },
+  // { text: 'NOTICIAS', href: '/noticias' },
+  { text: 'SOFTWARE', href: '/software' },
   { text: 'GAMING', href: '/gaming' },
   { text: 'CINE', href: '/cine' },
   { text: 'MÚSICA', href: '/musica' },
+  { text: 'LECTURA', href: '/lectura' },
   { text: 'CONTACTO', href: '/contacto' },
+
 ];
