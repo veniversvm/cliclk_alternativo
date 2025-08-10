7ade8941830c094584b00a11496f058287f7c76a
FranSabt <hsabatino1@gmail.com>
2025-08-10 17:49:40 -0400

    FAQ y About us

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,13 +1,15 @@
 export const navLinks = [
-  { text: 'INICIO', href: '/' },
-  { text: 'TODO', href: '/todo' },
+  // { text: 'INICIO', href: '/' },
+  // { text: 'TODO', href: '/todo' },
   { text: 'PÁGINAS', href: '/paginas' },
-  // { text: 'NOTICIAS', href: '/noticias' },
+  { text: 'NOTICIAS', href: '/noticias' },
   { text: 'SOFTWARE', href: '/software' },
   { text: 'GAMING', href: '/gaming' },
   { text: 'CINE', href: '/cine' },
   { text: 'MÚSICA', href: '/musica' },
   { text: 'LECTURA', href: '/lectura' },
-  { text: 'CONTACTO', href: '/contacto' },
+  // { text: 'CONTACTO', href: '/contacto' },
+  { text: 'SOBRE NOSOTROS', href: '/sobre-nosotros' },
+  { text: 'FAQ', href: '/faq' },
 
 ];
